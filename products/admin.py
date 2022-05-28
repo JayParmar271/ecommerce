b9d2551abca93f65fa2b706ff6146764ab9407e1
jayparmar271 <jayparmar271@gmail.com>
2022-05-28 21:05:20 +0530

    Save logged in admin details

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'qty', 'is_active', 'created')
+    list_display = ('name', 'user', 'category', 'price', 'qty', 'is_active', 'created')
+    exclude = ('user',)
 
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active', 'created')
 
