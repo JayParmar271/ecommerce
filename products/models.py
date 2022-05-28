89719674a3d09ddbeb3403ecbc371ea80938f7e4
jayparmar271 <jayparmar271@gmail.com>
2022-05-28 16:24:41 +0530

    Add: Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=30)
+    price = models.FloatField(null=False)
+    qty = models.IntegerField(null=False)
