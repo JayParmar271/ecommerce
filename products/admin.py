6984cc461f8bd7ee3da007374977c7535c3e4562
jayparmar271 <jayparmar271@gmail.com>
2022-05-28 21:11:28 +0530

    Added actions

--- products/admin.py
+++ products/admin.py
@@ -8,6 +8,17 @@ class ProductAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         return super().save_model(request, obj, form, change)
+
+    @admin.action(description='Mark selected product to active')
+    def mark_to_active(modeladmin, request, queryset):
+        queryset.update(is_active=True)
+
+    @admin.action(description='Mark selected product to inactive')
+    def mark_to_inactive(modeladmin, request, queryset):
+        queryset.update(is_active=False)
+
+    actions = [mark_to_active, mark_to_inactive]
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active', 'created')
 
--- products/models.py
+++ products/models.py
@@ -1,12 +1,13 @@
 from statistics import mode
 from unicodedata import category
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import User
 from app.mixins.timestampable import TimestampableMixin
 from app.mixins.status import StatusMixin
 from app.mixins.str import StrMixin
 
-class Product(TimestampableMixin, StatusMixin):
+class Product(TimestampableMixin, StatusMixin, StrMixin):
     name = models.CharField(max_length=30)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
